3f509edfcefaf8d9f91e5a846bb49f298273686c
Cajunvoodoo <27892784+Cajunvoodoo@users.noreply.github.com>
2025-07-02 21:15:09 -0400

    Swap to servant-client because req sucks

--- app/Main.hs
+++ app/Main.hs
@@ -4,11 +4,31 @@ import Control.Monad (unless)
 import Control.Monad.IO.Class
 import Data.ByteString (ByteString)
 import Data.ByteString.Char8 qualified as BS
+import Data.ByteString.Lazy qualified as BL (fromStrict, toStrict)
+import Data.Functor.Alt ((<!>))
+import Data.Proxy (Proxy (..))
 import Data.String.Interpolate (i)
 import Data.Text qualified as T
 import Data.Time (defaultTimeLocale, formatTime, getCurrentTime)
 import Data.Time.LocalTime
-import Network.HTTP.Req
+import Network.HTTP.Client (newManager)
+import Network.HTTP.Client.TLS (tlsManagerSettings)
+import Network.HTTP.Media qualified as M
+import Servant.API
+  ( Accept (..)
+  , Capture
+  , Get
+  , MimeRender (..)
+  , MimeUnrender (..)
+  , (:>)
+  )
+import Servant.Client
+  ( ClientM
+  , client
+  , mkClientEnv
+  , parseBaseUrl
+  )
+import Servant.Client.Internal.HttpClient (runClientM)
 import System.Exit (die)
 import System.Posix.ByteString
   ( fileExist
@@ -38,8 +58,13 @@ main = do
             :: T.Text
         )
     else do
-      imageData <- runReq defaultHttpConfig $ requestImage year month
-      swapCurrentWp year month homeDir imageData
+      manager <- newManager tlsManagerSettings
+      baseUrl <- parseBaseUrl "kriegs.net"
+      reqResult <-
+        flip runClientM (mkClientEnv manager baseUrl) $ requestImage year month
+      case reqResult of
+        Left err -> print ([i|Error: #{err}|] :: T.Text)
+        Right imageData -> swapCurrentWp year month homeDir imageData
 
 firstArg :: [a] -> IO a
 firstArg [] =
@@ -53,22 +78,53 @@ mkYearMonth lt = do
       monthStr = fmtDate "%B"
   (yearStr, monthStr)
 
-requestImage :: (MonadHttp m, MonadIO m) => Year -> Month -> m ByteString
+requestImage :: Year -> Month -> ClientM ByteString
 requestImage year month = do
-  let imageUrl =
-        https "www.kriegs.net"
-          /: "work"
-          /: year
-          /: "wallpapers"
-          /: [i|kriegs_#{year}_#{month}_4K_3840x2160_calendar.jpg|]
-  r <-
-    req
-      GET -- method
-      imageUrl -- url
-      NoReqBody -- no req body
-      bsResponse -- bytestring
-      mempty -- query params, headers, etc
-  pure $ responseBody r
+  let seps :: [T.Text] = ["-", "_"] -- Weird change he made
+  -- let imageUrl =
+  --       https "www.kriegs.net"
+  --         /: "work"
+  --         /: year
+  --         /: "wallpapers"
+  --         /: [i|kriegs_#{year}_#{month}_4K_3840x2160_calendar.jpg|]
+  let imageUrls =
+        fmap (\sep -> [i|kriegs_#{year}_#{month}_4K_3840x2160#{sep}calendar.jpg|]) seps
+  let reqs = fmap (requestImage' year) imageUrls
+  r <- foldr1 (<!>) reqs
+  -- r <-
+  --   req
+  --     GET -- method
+  --     imageUrl -- url
+  --     NoReqBody -- no req body
+  --     bsResponse -- bytestring
+  --     mempty -- query params, headers, etc
+  pure r
+
+-- pure $ responseBody r
+
+data JPEG
+
+instance Accept JPEG where
+  contentType _ = "image" M.// "jpeg"
+
+instance MimeRender JPEG ByteString where
+  mimeRender _ = BL.fromStrict
+
+instance MimeUnrender JPEG ByteString where
+  mimeUnrender _ = Right . BL.toStrict
+
+type KriegsAPI =
+  "work"
+    :> Capture "year" Year
+    :> "wallpapers"
+    :> Capture "slug" T.Text
+    :> Get '[JPEG] ByteString
+
+api :: Proxy KriegsAPI
+api = Proxy
+
+requestImage' :: Year -> T.Text -> ClientM ByteString
+requestImage' = client api
 
 swapCurrentWp :: (MonadIO m) => Year -> Month -> HomeDir -> ByteString -> m ()
 swapCurrentWp year month homeDir imgData = do
